38c57bf4e9dabe457093ad986556b408645b1d46
Emily Luong <emily.luong03@hotmail.com>
2021-01-17 16:58:49 -0500

    updated styling

--- client/src/components/Messaging/Navigation/Navigation.js
+++ client/src/components/Messaging/Navigation/Navigation.js
@@ -2,10 +2,14 @@ import React from 'react';
 import classes from './Navigation.module.css';
 
 const navigation = (props) => {
+    const logoutHandler = () => {
+        localStorage.removeItem('token');
+    }
+
     return (
         <div className={classes.Navigation}>
             <h1>ChatApp</h1>
-            <a href='#'><b>LOGOUT</b></a>
+            <a href='/signin' onClick={logoutHandler}><b>LOGOUT</b></a>
         </div>
     );
 };
